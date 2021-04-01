2ff75c06f17031484a8ebcf4f651423ec09c04ee
Arno Schutijzer <arno.schutijzer@skryv.com>
2021-04-01 21:19:46 +0200

    feat: add server implementation

--- server.go
+++ server.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+const PORT_ENV_KEY = "PORT"
+
 func main() {
-	panic("Not implemented")
+	server := CreateServer()
+	server.ListenAndServe()
+}
+
+func CreateServer() *http.Server {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", HealthcheckHandler)
+	mux.HandleFunc("/api/health", HealthcheckHandler)
+
+	server := &http.Server{
+		Addr:    GetAddress(),
+		Handler: mux,
+	}
+
+	return server
+}
+
+func GetAddress() string {
+	var port string
+
+	value, ok := os.LookupEnv(PORT_ENV_KEY)
+
+	if !ok {
+		port = "8080"
+	} else {
+		port = value
+	}
+
+	return fmt.Sprintf(":%s", port)
+}
+
+func HealthcheckHandler(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(200)
 }
--- server_test.go
+++ server_test.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Main_Panics(t *testing.T) {
-	assert.Panics(t, func() { main() })
+func TestServerReturnsOkOnRootAndHealthcheck(t *testing.T) {
+	for _, uri := range []string{
+		"/",
+		"/api/health",
+	} {
+		req, _ := http.NewRequest(http.MethodGet, uri, nil)
+
+		requestRecorder := httptest.NewRecorder()
+		handler := http.HandlerFunc(HealthcheckHandler)
+
+		handler.ServeHTTP(requestRecorder, req)
+
+		assert.Equal(t, http.StatusOK, requestRecorder.Code)
+	}
+}
+
+func TestGetPortWhenNotSetReturns8080(t *testing.T) {
+	assert.Equal(t, ":8080", GetAddress())
+}
+
+func TestGetPortWhen5000SetReturns5000(t *testing.T) {
+	os.Setenv("PORT", "5000")
+	assert.Equal(t, ":5000", GetAddress())
 }
